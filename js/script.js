a1816f31bda8953210be68bcc94f35a40b777882
NanhoiRey <nanhoirey94@gmail.com>
2025-11-16 13:55:47 +0100

    Correcion de Section + Fotos INDEX

--- js/script.js
+++ js/script.js
@@ -67,3 +67,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
   setInterval(changeSlide, 5000);
 });
+
+
+// LIGHTBOX
+
+const galleryItems = document.querySelectorAll('.gallery-item img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+galleryItems.forEach(img => {
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'block';
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = 'none';
+  }
+});
